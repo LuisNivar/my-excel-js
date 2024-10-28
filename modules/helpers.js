aef611bafd2e8cf17c8b0dd0a9432710725379f3
LuisNivar <45906349+LuisNivar@users.noreply.github.com>
2024-10-28 16:55:16 -0400

    Range (#9)
    * -Refactor selection code
    -Refactor formatting code
    
    * Allow use range excel notation
    
    * Fix custom formulas
    
    * Allow resize columns
    
    * Add basic footer

--- modules/helpers.js
+++ modules/helpers.js
@@ -20,24 +20,68 @@ const computeValue = (value, constants) => {
     return value;
   }
 
-  const formula = value.slice(1);
+  let formula = value.slice(1);
   let computedValue;
 
   const excelFormulas = [
-    "const sum = (...args) => [...args].reduce((a, b) => a + b, 0);",
-    "const today = () => new Date(Date.now()).toLocaleDateString();",
+    `function sum(...args) {
+      let result = 0;
+      if ([...args].length === 0) return 0;
+      for (const arg of [...args]) {
+        const n = isNaN(arg) ? 0 : arg;
+        result += n;
+      }
+      return result;
+    }`,
+    `function average(...args) {
+    const n = [...args].length;
+    const result = sum(...args)/n;
+    return result;
+  }`,
+    `function today(){
+    const now = new Date(Date.now());
+    return now.toLocaleDateString();
+  }`,
+    `  function max(...args){
+    return Math.max(...args);
+  }`,
+    ` function min(...args){
+    return Math.min(...args);
+  }`,
+    ` function concat(...args){
+    if ([...args].length === 0) return "";
+    let result = "";
+    for (const arg of [...args]) {
+      result += arg;
+    }
+    return result;
+  }`,
   ];
+
+  const rangePattern = /[a-zA-Z]\d:[a-zA-Z]\d/;
+  const hasRange = rangePattern.test(formula);
+
+  if (hasRange) {
+    const rangeCells = formula.match(rangePattern)[0];
+    const rangeConst = range2Consts(rangeCells);
+    formula = formula.replace(rangePattern, rangeConst);
+  }
 
   const formulaFormated = formula.toLowerCase();
 
   try {
     computedValue = new Function(`
             ${constants}
-            ${excelFormulas.join("")}
-
+            ${excelFormulas.join("\n")}
+            
             return ${formulaFormated}`)();
   } catch (e) {
-    computedValue = "#ERROR!"; //+ e.message';
+    computedValue = "#ERROR!"; // + e.message;
+  }
+
+  // One Decimal place
+  if (!isNaN(computedValue)) {
+    computedValue = Math.round(computedValue * 10) / 10;
   }
 
   return computedValue;
@@ -46,13 +90,54 @@ const computeValue = (value, constants) => {
 const selection = {
   column: null,
   row: null,
+  get isColumnSelected() {
+    return this.column !== null && this.row === null;
+  },
+  get isRowSelected() {
+    return this.column === null && this.row !== null;
+  },
+  get isEmpty() {
+    return this.column === null && this.row === null;
+  },
+  get isCellSelected() {
+    return !this.isColumnSelected && !this.isRowSelected && !this.isEmpty;
+  },
+  clear() {
+    this.column = null;
+    this.row = null;
+  },
 };
 
 function removeSelection() {
   getElements(".selected").forEach((el) => el.classList.remove("selected"));
-  selection.column = null;
-  selection.row = null;
+  selection.clear();
 }
+
+const range2Consts = (rangeCells) => {
+  const start = /(?<column>[a-zA-Z])(?<row>\d)(?=:)/.exec(rangeCells).groups;
+  const end = /(?<=:)(?<column>[a-zA-Z])(?<row>\d)/.exec(rangeCells).groups;
+
+  const startRow = Number(start.row);
+  const endRow = Number(end.row);
+
+  const startColum =
+    start.column.toUpperCase().charCodeAt(0) - FIRST_LETTER_ASCII_CODE;
+
+  const endColum =
+    end.column.toUpperCase().charCodeAt(0) - FIRST_LETTER_ASCII_CODE;
+
+  let arrayCells = [];
+  for (let j = startColum; j < endColum + 1; j++) {
+    const letter = getColumnLetter(j);
+
+    for (let i = startRow; i < endRow + 1; i++) {
+      arrayCells.push(letter + i);
+    }
+  }
+
+  const result = arrayCells.join(",");
+  return result;
+};
 
 const computeAllCells = (cells, constants) => {
   cells.forEach((rows, x) => {
